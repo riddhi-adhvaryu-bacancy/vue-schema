55805f89e2cd0fd7b23c5add7332069cf285d012
radhvaryu <radhvaryu@nalu.ai>
2021-11-25 18:23:52 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <SchemaForm/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import SchemaForm from './components/SchemaForm';
+
+export default {
+  name: 'App',
+
+  components: {
+    SchemaForm,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/SchemaForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+    <h1>Create Form using json-schema</h1>
+    <v-jsonschema-form
+      v-if="
+        schemaData &&
+          Object.keys(schemaData).length > 0
+      "
+      :schema="schemaData"
+      :model="modelData"
+      :options="schemaOptions"
+    />
+    <v-btn color="light-blue" @click="saveJson">Save</v-btn
+    >
+  </v-container>
+</template>
+
+<script>
+
+  import Vue from 'vue'
+  import Vuetify from 'vuetify'
+  import 'vuetify/dist/vuetify.min.css'
+  import Draggable from 'vuedraggable'
+  import Swatches from 'vue-swatches'
+  import 'vue-swatches/dist/vue-swatches.min.css'
+  import VJsonschemaForm from '@koumoul/vuetify-jsonschema-form'
+  import '@koumoul/vuetify-jsonschema-form/dist/main.css'
+  import { Sketch } from 'vue-color'
+
+  Vue.use(Vuetify)
+
+  Vue.component('swatches', Swatches)
+  Vue.component('draggable', Draggable)
+  Vue.component('color-picker', Sketch)
+
+  export default {
+    name: 'SchemaForm',
+    components: {
+      VJsonschemaForm,
+    },
+    data: () => ({
+      schemaData: require("./schema"),
+      modelData: {},
+      schemaOptions: {
+        "accordionMode": "normal"
+      }
+    }),
+    methods: {
+      saveJson() {
+        console.log("modelData",this.modelData)
+      }
+    }
+  }
+</script>
